08488df0306874e6d170003d35d3fc5e0332463d
Connell Leon <143096904+caleon0306@users.noreply.github.com>
2025-02-13 14:49:25 -0500

    update test)leagues to test that it initializes correctly and will return correct errors when an invalid league is looked up

--- test_files/scratch.py
+++ test_files/scratch.py
@@ -4,8 +4,15 @@ from sleeper_api import base_api
 from sleeper_api import Users
 from sleeper_api import Leagues
 
-
+#python -m test_files.scratch
 
 if __name__ == "__main__":
     user = "csonal"
+    leagueID = "1180209400990347264"
+
     userOBJ = Users.User(user)
+    
+    league = Leagues.League("-12")
+    print(type(league.getAllLeagueInfo()))
+    
+    
--- test_files/test_leagues.py
+++ test_files/test_leagues.py
@@ -1,14 +1,26 @@
 import unittest
 
+from requests.exceptions import HTTPError
 from sleeper_api.Leagues import League
 
 class testLeagues(unittest.TestCase):
+
+    #test to make sure the league initializes correctly
     def testLeagueInitialization(self):
         leagueID = "1180209400990347264"
         leagueOBJ = League(leagueID)
 
         self.assertEqual(leagueOBJ.league_id, leagueID)
         self.assertEqual(leagueOBJ.getAllLeagueInfo()["league_id"], leagueID)
+
+    #test attempting to get information from an invalid league
+    def testInvalidLeague(self):
+        leagueId = "-12"
+        leagueOBJ = League(leagueId)
+
+        self.assertIsInstance(leagueOBJ.getAllLeagueInfo(), HTTPError)
+        self.assertEqual(leagueOBJ.getRosters(), None)
+        self.assertEqual(leagueOBJ.getUsers(), [])
 
 if __name__ == '__main__':
     unittest.main()
